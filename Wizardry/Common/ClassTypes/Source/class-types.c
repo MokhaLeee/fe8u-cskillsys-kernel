2b0cf0c40323706450061ba62b3d312d6b6cd32c
MokhaLeee <nkulyc@163.com>
2024-05-29 17:39:58 +0800

    fasten function: CheckSameClassType()

--- Wizardry/Common/ClassTypes/Source/class-types.c
+++ Wizardry/Common/ClassTypes/Source/class-types.c
@@ -7,39 +7,42 @@ extern u8 const * const gpKernelClassList_Armor;
 extern u8 const * const gpKernelClassList_Dragon;
 extern u8 const * const gpKernelClassList_Beast;
 
+typedef bool (*ClassTypeFunc_t)(u8 jid);
+
 bool CheckSameClassType(u8 jid1, u8 jid2)
 {
-    bool jdg1, jdg2;
+    static const u8 jdg_list[] = {
+        2, // 00
+        0, // 01
+        0, // 10
+        1, // 11
+    };
 
-    jdg1 = CheckClassFlier(jid1);
-    jdg2 = CheckClassFlier(jid2);
-    if (jdg1 == true || jdg2 == true)
-        if (jdg1 != jdg2)
+    static const ClassTypeFunc_t jdg_funcs[] = {
+        CheckClassFlier,
+        CheckClassCavalry,
+        CheckClassArmor,
+        CheckClassDragon,
+        CheckClassBeast,
+        NULL
+    };
+
+    const ClassTypeFunc_t *it;
+
+    for (it = jdg_funcs; *it; it++)
+    {
+        switch (jdg_list[((*it)(jid1) << 1) | (*it)(jid2)]) {
+        case 1:
+            return true;
+
+        case 0:
             return false;
 
-    jdg1 = CheckClassCavalry(jid1);
-    jdg2 = CheckClassCavalry(jid2);
-    if (jdg1 == true || jdg2 == true)
-        if (jdg1 != jdg2)
-            return false;
-
-    jdg1 = CheckClassArmor(jid1);
-    jdg2 = CheckClassArmor(jid2);
-    if (jdg1 == true || jdg2 == true)
-        if (jdg1 != jdg2)
-            return false;
-
-    jdg1 = CheckClassDragon(jid1);
-    jdg2 = CheckClassDragon(jid2);
-    if (jdg1 == true || jdg2 == true)
-        if (jdg1 != jdg2)
-            return false;
-
-    jdg1 = CheckClassBeast(jid1);
-    jdg2 = CheckClassBeast(jid2);
-    if (jdg1 == true || jdg2 == true)
-        if (jdg1 != jdg2)
-            return false;
+        case 2:
+        default:
+            break;
+        }
+    }
 
     /* Both of which are normal jobs */
     return true;
