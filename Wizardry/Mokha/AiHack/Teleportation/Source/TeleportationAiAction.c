19830598fd7d466e6e6ca0e5b7fa9f6dd6314177
MokhaLeee <nkulyc@163.com>
2024-06-01 06:21:45 +0000

    TeleportationAiAction done

--- Wizardry/Mokha/AiHack/Teleportation/Source/TeleportationAiAction.c
+++ Wizardry/Mokha/AiHack/Teleportation/Source/TeleportationAiAction.c
@@ -1,129 +1,119 @@
 #include "common-chax.h"
+#include "kernel-lib.h"
 
 #define LOCAL_TRACE 1
 
-STATIC_DECLAR void TeleportationAction_MoveCameraOnSubject(ProcPtr proc)
+STATIC_DECLAR void clear_mus(void)
 {
-    EnsureCameraOntoPosition(proc, gActiveUnit->xPos, gActiveUnit->yPos);
+    MU_EndAll();
+    RefreshUnitSprites();
 }
 
-STATIC_DECLAR void TeleportationAction_AnimateSubjectIdle(ProcPtr proc)
+static void set_actor_unit(void)
 {
-    struct MUProc * mu = MU_GetByUnit(gActiveUnit);
-    if (!mu)
-        mu = MU_Create(gActiveUnit);
-
-    MU_StartActionAnim(mu);
+    gEventSlots[EVT_SLOT_2] = UNIT_CHAR_ID(gActiveUnit);
 }
 
-void TeleportationAction_SelfWarpOutMuHide(ProcPtr proc)
+static void set_position(void)
 {
-    MU_Hide(MU_GetByUnit(gActiveUnit));
+    gActiveUnit->xPos = gActionData.xMove;
+	gActiveUnit->yPos = gActionData.yMove;
 }
 
-const struct ProcCmd ProcScr_SpellAssocSelfWarpMuHide[] = {
-    PROC_SLEEP(0x5),
-    PROC_CALL(TeleportationAction_SelfWarpOutMuHide),
-    PROC_END
-};
+STATIC_DECLAR const EventScr EventScr_ActionTeleportation[] = {
+    EVBIT_MODIFY(0x4)
+    STAL(20)
 
-STATIC_DECLAR void TeleportationAction_SelfWarpOut(ProcPtr proc)
-{
-    LTRACEF("WarpOut at x=%d, y=%d", gActiveUnit->xPos, gActiveUnit->yPos);
+LABEL(0)
+    ASMC(clear_mus)
+    ASMC(set_actor_unit)
+    CALL(EventScr_UnitWarpOUT)
+    STAL(60)
+    ASMC(set_position)
+    ASMC(set_actor_unit)
+    CALL(EventScr_UnitWarpIN)
+    STAL(20)
 
-    StartEventWarpAnim(proc, gActiveUnit->xPos, gActiveUnit->yPos, 0, 0);
-    Proc_Start(ProcScr_SpellAssocSelfWarpMuHide, proc);
-}
-
-void TeleportationAction_SelfWarpInUpdateMu(ProcPtr proc)
-{
-    int x = gActionData.xMove;
-    int y = gActionData.yMove;
-    int ix = x * 16 - gBmSt.camera.x;
-    int iy = y * 16 - gBmSt.camera.y;
-
-    struct MUProc * pmu = MU_GetByUnit(gActiveUnit);
-
-    gActiveUnit->xPos = x;
-    gActiveUnit->yPos = y;
-
-    MU_SetDisplayPosition(pmu, ix, iy);
-}
-
-void TeleportationAction_SelfWarpOutMuShow(ProcPtr proc)
-{
-    MU_Show(MU_GetByUnit(gActiveUnit));
-}
-
-const struct ProcCmd ProcScr_SpellAssocSelfWarpMuShow[] = {
-    PROC_SLEEP(0x14),
-    PROC_CALL(TeleportationAction_SelfWarpOutMuShow),
-    PROC_END
-};
-
-void TeleportationAction_SelfWarpIn(ProcPtr proc)
-{
-    LTRACEF("WarpIn at x=%d, y=%d", gActiveUnit->xPos, gActiveUnit->yPos);
-
-    StartEventWarpAnim(proc, gActiveUnit->xPos, gActiveUnit->xPos, 1, 0);
-    Proc_Start(ProcScr_SpellAssocSelfWarpMuShow, proc);
-}
-
-void TeleportationAction_SubjectResetAnim(ProcPtr proc)
-{
-    MU_StartDelayedFaceTarget(MU_GetByUnit(gActiveUnit));
-}
-
-const struct ProcCmd ProcScr_AiActionTeleportation[] = {
-    PROC_CALL(TeleportationAction_MoveCameraOnSubject),
-    PROC_SLEEP(0x2),
-    PROC_CALL(TeleportationAction_AnimateSubjectIdle),
-    PROC_SLEEP(0x1E),
-    PROC_CALL(TeleportationAction_SelfWarpOut),
-    PROC_WHILE(EventWarpAnimExists),
-    PROC_SLEEP(0xA),
-    PROC_CALL(TeleportationAction_SelfWarpInUpdateMu),
-    PROC_CALL(TeleportationAction_MoveCameraOnSubject),
-    PROC_SLEEP(0xA),
-    PROC_CALL(TeleportationAction_SelfWarpIn),
-    PROC_WHILE(EventWarpAnimExists),
-    PROC_CALL(SetDefaultColorEffects),
-    PROC_SLEEP(0x2),
-    PROC_CALL(TeleportationAction_SubjectResetAnim),
-    PROC_SLEEP(0x1E),
-    PROC_END
+LABEL(99)
+    NoFade
+    ENDA
 };
 
 /* Action */
 bool Action_Teleportation(ProcPtr parent)
 {
-    Proc_StartBlocking(ProcScr_AiActionTeleportation, parent);
+	LTRACEF("Teleportation AiAction (uid=%x) to x=%d, y=%d",
+		gActiveUnit->index & 0xFF, gActionData.xMove, gActionData.xMove);
+
+    KernelCallEvent(EventScr_ActionTeleportation, EV_EXEC_CUTSCENE, parent);
     return false;
 }
 
 /* AiActionConf::exec */
 void AiAction_Teleportation(ProcPtr parent)
 {
-    LTRACEF("Teleportation AiAction to x=%d, y=%d", gAiDecision.xTarget, gAiDecision.yTarget);
-
     gActionData.xMove = gAiDecision.xTarget;
     gActionData.yMove = gAiDecision.yTarget;
 
-    Proc_StartBlocking(ProcScr_AiActionTeleportation, parent);
+	Action_Teleportation(parent);
 }
 
 /* AiActionConf:idle */
-bool AiAction_TeleportationIdle(ProcPtr parent)
+STATIC_DECLAR bool AiAction_TeleportationIdleExt(struct CpPerformProc * proc)
 {
-    if (Proc_Find(ProcScr_AiActionTeleportation) != NULL)
-        return false;
-
-    LTRACEF("Teleportation AiAction done at x=%d, y=%d", gActiveUnit->xPos, gActiveUnit->yPos);
-
     gAiDecision.xMove = gAiDecision.xTarget;
     gAiDecision.yMove = gAiDecision.yTarget;
 
     gActiveUnit->state &= ~US_HAS_MOVED_AI;
     gAiState.unitIt--;
     return true;
+}
+
+bool AiAction_TeleportationIdle(struct CpPerformProc * proc)
+{
+	/* Force introduce a YIELD */
+	proc->func = AiAction_TeleportationIdleExt;
+	return false;
+}
+
+/* LynJump */
+void CpPerform_Cleanup(struct CpPerformProc * proc)
+{
+    UpdateAllPhaseHealingAIStatus();
+
+#if !CHAX
+    AiRefreshMap();
+#else
+	gActiveUnit = GetUnit(gActionData.subjectIndex);
+
+    SetCursorMapPosition(gAiDecision.xMove, gAiDecision.yMove);
+    RenderBmMapOnBg2();
+
+    MoveActiveUnit(gAiDecision.xMove, gAiDecision.yMove);
+
+#if CHAX
+	if (gActiveUnit->curHP != 0)
+	{
+		switch (gAiDecision.actionId) {
+		case CONFIG_AI_ACTION_EXPA_Teleportation:
+			gActiveUnit->state &= ~US_UNSELECTABLE;
+			break;
+		}
+	}
+#endif
+
+    RefreshEntityBmMaps();
+    RenderBmMap();
+
+    NewBMXFADE(1);
+
+    MU_EndAll();
+    RefreshEntityBmMaps();
+
+    ShowUnitSprite(gActiveUnit);
+    RefreshUnitSprites();
+#endif
+
+    if (!(gActiveUnit->pCharacterData) || (gActiveUnit->state & (US_HIDDEN | US_DEAD | US_BIT16)))
+        Proc_Goto(proc, 1);
 }
