4e58eaf475d431d5f25e18f5d45f903ff066481c
MokhaLeee <nkulyc@163.com>
2024-06-01 10:19:38 +0000

    optimize skill-tester fastfast

--- Wizardry/Core/SkillSys/kernel/JudgeSkillFast.c
+++ Wizardry/Core/SkillSys/kernel/JudgeSkillFast.c
@@ -17,24 +17,28 @@ bool SkillTesterFastFast(struct Unit * unit, const u8 sid)
 
     int ref;
     u8 pid, jid;
+    const u8 * list;
 
-    CHECK_SKILL(UNIT_RAM_SKILLS(unit)[0], sid);
-    CHECK_SKILL(UNIT_RAM_SKILLS(unit)[1], sid);
-    CHECK_SKILL(UNIT_RAM_SKILLS(unit)[2], sid);
-    CHECK_SKILL(UNIT_RAM_SKILLS(unit)[3], sid);
-    CHECK_SKILL(UNIT_RAM_SKILLS(unit)[4], sid);
-    CHECK_SKILL(UNIT_RAM_SKILLS(unit)[5], sid);
-    CHECK_SKILL(UNIT_RAM_SKILLS(unit)[6], sid);
-
-    pid = UNIT_CHAR_ID(unit);
-    ref = pid * 2;
-    CHECK_SKILL(gpConstSkillPTable[ref + 0], sid);
-    CHECK_SKILL(gpConstSkillPTable[ref + 1], sid);
+    list = UNIT_RAM_SKILLS(unit);
+    CHECK_SKILL(list[0], sid);
+    CHECK_SKILL(list[1], sid);
+    CHECK_SKILL(list[2], sid);
+    CHECK_SKILL(list[3], sid);
+    CHECK_SKILL(list[4], sid);
+    CHECK_SKILL(list[5], sid);
+    CHECK_SKILL(list[6], sid);
 
     jid = UNIT_CLASS_ID(unit);
     ref = jid * 2;
-    CHECK_SKILL(gpConstSkillJTable[ref + 0], sid);
-    CHECK_SKILL(gpConstSkillJTable[ref + 1], sid);
+    list = gpConstSkillJTable;
+    CHECK_SKILL(list[ref + 0], sid);
+    CHECK_SKILL(list[ref + 1], sid);
+
+    pid = UNIT_CHAR_ID(unit);
+    ref = pid * 2;
+    list = gpConstSkillPTable;
+    CHECK_SKILL(list[ref + 0], sid);
+    CHECK_SKILL(list[ref + 1], sid);
 
     return false;
 }
