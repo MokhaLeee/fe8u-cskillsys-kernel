6c45de69f98441a8b4170ea8030b57ffaecb543d
JesterWizard <164426396+JesterWizard@users.noreply.github.com>
2024-06-02 18:48:22 +0100

    Added spaces for // comments and if statement

--- Wizardry/Core/BattleSys/Source/PreBattleCalc.c
+++ Wizardry/Core/BattleSys/Source/PreBattleCalc.c
@@ -432,9 +432,9 @@ STATIC_DECLAR void PreBattlePostCalcSkills(struct BattleUnit * attacker, struct 
 #if (defined(SID_Merciless) && (SID_Merciless < MAX_SKILL_NUM))
     if (SkillTester(unit, SID_Merciless))
     {
-        //Check if the defending unit has the poison status
-        if(GetUnitStatusIndex(&defender->unit) == UNIT_STATUS_POISON)
-            //If so, then set an arbitrary high value for crit to 'gurantee' it.
+        // Check if the defending unit has the poison status
+        if (GetUnitStatusIndex(&defender->unit) == UNIT_STATUS_POISON)
+            // If so, then set an arbitrary high value for crit to 'gurantee' it.
             attacker->battleCritRate = 255;
     }
 #endif
