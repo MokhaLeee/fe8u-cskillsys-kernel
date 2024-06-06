a516ebd7abb395ff01fc4436fde9fdc2096e7aae
MokhaLeee <nkulyc@163.com>
2024-06-06 17:41:14 +0000

    fix unique anim

--- Tools/scripts/generate_release.sh
+++ Tools/scripts/generate_release.sh
@@ -7,8 +7,9 @@ function collect_header() {
     rm -rf $COMMON_HEADER
 
     echo "#pragma once" > $COMMON_HEADER
+    echo "#include <common-chax.h>" > $COMMON_HEADER
 
-    for file in $(find ./kernel -type f -name "*.h")
+    for file in $(find -type f -name "*.h")
     do
         echo "#include <${file#*/}>" >> $COMMON_HEADER
     done
@@ -20,25 +21,23 @@ RELEASE_DIR=.release_dir
 
 rm -rf $RELEASE_DIR
 mkdir $RELEASE_DIR
-mkdir $RELEASE_DIR/include
-mkdir $RELEASE_DIR/include/kernel
 
 # make -j
 cp fe8-kernel-* $RELEASE_DIR
-cp -rf include/* $RELEASE_DIR/include/kernel
+cp -rf include $RELEASE_DIR
 cp -rf Patches $RELEASE_DIR
 
 # fix texts
-TARGET_TEXTS_H=$RELEASE_DIR/include/kernel/constants/texts.h
-cp Contants/Texts/TextDefinitions.h $RELEASE_DIR/include/kernel/constants/
+TARGET_TEXTS_H=$RELEASE_DIR/include/constants/texts.h
+cp Contants/Texts/TextDefinitions.h $RELEASE_DIR/include/constants/
 echo '#ifndef TEXTS_H' > $TARGET_TEXTS_H
 echo '#define TEXTS_H' >> $TARGET_TEXTS_H
 echo '#include "TextDefinitions.h"' >> $TARGET_TEXTS_H
 echo '#endif // TEXTS_H' >> $TARGET_TEXTS_H
 
 # fix gfx
-TARGET_GFX_H=$RELEASE_DIR/include/kernel/constants/gfx.h
-cp Contants/Gfx/GfxDefs.h $RELEASE_DIR/include/kernel/constants/
+TARGET_GFX_H=$RELEASE_DIR/include/constants/gfx.h
+cp Contants/Gfx/GfxDefs.h $RELEASE_DIR/include/constants/
 echo '#ifndef GFX_H' > $TARGET_GFX_H
 echo '#define GFX_H' >> $TARGET_GFX_H
 echo '#include "GfxDefs.h"' >> $TARGET_GFX_H
