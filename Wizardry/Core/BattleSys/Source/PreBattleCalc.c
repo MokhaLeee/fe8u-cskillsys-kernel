c6912ff32ce59129a10bb50abceb9fb699021786
MokhaLeee <nkulyc@163.com>
2024-06-07 11:37:03 +0800

    update code style issue

--- Wizardry/Core/BattleSys/Source/PreBattleCalc.c
+++ Wizardry/Core/BattleSys/Source/PreBattleCalc.c
@@ -434,9 +434,13 @@ void PreBattleCalcSkills(struct BattleUnit * attacker, struct BattleUnit * defen
 
 #if (defined(SID_CriticalPierce) && (SID_CriticalPierce < MAX_SKILL_NUM))
     if (SkillTester(unit, SID_CriticalPierce))
-        // Add the defender's critical avoid rate to the attacker's.
-        // Thus effectively 'negating' it.
+    {
+        /**
+         * Add the defender's critical avoid rate to the attacker's.
+         * Thus effectively 'negating' it.
+         */
         attacker->battleCritRate += defender->battleDodgeRate;
+    }
 #endif
 
 #if (defined(SID_KillingMachine) && (SID_KillingMachine < MAX_SKILL_NUM))
