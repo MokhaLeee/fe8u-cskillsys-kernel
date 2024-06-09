857dcfbe167c77f0c40d6cc91b0acaaff806c6cb
JesterWizard <164426396+JesterWizard@users.noreply.github.com>
2024-06-09 09:27:49 +0100

    Skill - LightWeight

--- Data/SkillSys/SkillInfo.c
+++ Data/SkillSys/SkillInfo.c
@@ -1614,4 +1614,11 @@ const struct SkillInfo gSkillInfos[0x100] = {
          .icon = GFX_SkillIcon_WaterBoost,
       },
 #endif
+
+#if (defined(SID_LightWeight) && (SID_LightWeight < MAX_SKILL_NUM)) 
+    [SID_LightWeight] = {
+         .desc = MSG_SKILL_LightWeight,
+         .icon = GFX_SkillIcon_WIP,
+      },
+#endif
 };
