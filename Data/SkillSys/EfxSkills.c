858a3083a2fd7afeae829cf88b6f83ab314c78cb
JesterWizard <164426396+JesterWizard@users.noreply.github.com>
2024-06-17 21:03:02 +0100

    Added battle animation info

--- Data/SkillSys/EfxSkills.c
+++ Data/SkillSys/EfxSkills.c
@@ -197,4 +197,12 @@ const struct SkillAnimInfo gSkillAnimInfos[0x100] = {
         .sfx = 0x3D1,
     },
 #endif
+
+#if defined(SID_Impale) && (SID_Impale < MAX_SKILL_NUM)
+    [SID_Impale] = {
+        .aid = EFX_SKILL_DEFAULT,
+        .priority = EFX_PRIORITY_NORMAL,
+        .sfx = 0x3D1,
+    },
+#endif
 };
