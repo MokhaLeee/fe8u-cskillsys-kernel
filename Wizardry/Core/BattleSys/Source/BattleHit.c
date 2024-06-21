037718e80a2737efa164b74fb8bf611475de231e
JesterWizard <164426396+JesterWizard@users.noreply.github.com>
2024-06-21 22:41:33 +0100

    Reset activation rate

--- Wizardry/Core/BattleSys/Source/BattleHit.c
+++ Wizardry/Core/BattleSys/Source/BattleHit.c
@@ -213,7 +213,7 @@ void BattleGenerateHitAttributes(struct BattleUnit * attacker, struct BattleUnit
     if (damage > 0)
     {
 #if (defined(SID_GreatShield) && (SID_GreatShield < MAX_SKILL_NUM))
-            if (CheckBattleSkillActivte(defender, attacker, SID_GreatShield, 100))
+            if (CheckBattleSkillActivte(defender, attacker, SID_GreatShield, defender->unit.skl))
             {
                 RegisterTargetEfxSkill(GetBattleHitRound(gBattleHitIterator), SID_GreatShield);
                 damage = 0;
