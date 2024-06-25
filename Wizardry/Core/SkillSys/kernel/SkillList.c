ec60601ac9c9a61efd737d70bd9d96d324b38345
MokhaLeee <nkulyc@163.com>
2024-06-25 14:18:53 +0800

    fix skill list

--- Wizardry/Core/SkillSys/kernel/SkillList.c
+++ Wizardry/Core/SkillSys/kernel/SkillList.c
@@ -13,33 +13,40 @@ STATIC_DECLAR void GenerateSkillListExt(struct Unit * unit, struct SkillList * l
     int pid = UNIT_CHAR_ID(unit);
     int jid = UNIT_CLASS_ID(unit);
 
+    u8 * tmp_list = gGenericBuffer;
+
     memset(list, 0, sizeof(*list));
+    memset(tmp_list, 0, MAX_SKILL_NUM + 1);
 
     /* person */
     sid = gpConstSkillTable_Person[pid * 2];
     if (COMMON_SKILL_VALID(sid))
-        list->sid[list->amt++] = sid;
+        tmp_list[sid] = true;
 
     sid = gpConstSkillTable_Person[pid * 2 + 1];
     if (COMMON_SKILL_VALID(sid))
-        list->sid[list->amt++] = sid;
+        tmp_list[sid] = true;
 
     /* job */
     sid = gpConstSkillTable_Job[jid * 2];
     if (COMMON_SKILL_VALID(sid))
-        list->sid[list->amt++] = sid;
+        tmp_list[sid] = true;
 
     sid = gpConstSkillTable_Job[jid * 2 + 1];
     if (COMMON_SKILL_VALID(sid))
-        list->sid[list->amt++] = sid;
+        tmp_list[sid] = true;
 
     /* generic */
     for (i = 0; i < UNIT_RAM_SKILLS_LEN; i++)
     {
         sid = UNIT_RAM_SKILLS(unit)[i];
         if (COMMON_SKILL_VALID(sid))
-            list->sid[list->amt++] = sid;
+            tmp_list[sid] = true;
     }
+
+    for (i = 1; i < MAX_SKILL_NUM; i++)
+        if (tmp_list[i])
+            list->sid[list->amt++] = i;
 
     WriteUnitListHeader(unit, &list->header);
 }
