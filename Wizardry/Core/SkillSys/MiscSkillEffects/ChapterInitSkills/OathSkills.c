46cf74f0e9d242865703f1aa7c47d9bf59421d5b
MokhaLeee <nkulyc@163.com>
2024-06-30 04:12:07 +0000

    fix pr #158 game play design

--- Wizardry/Core/SkillSys/MiscSkillEffects/ChapterInitSkills/OathSkills.c
+++ Wizardry/Core/SkillSys/MiscSkillEffects/ChapterInitSkills/OathSkills.c
@@ -59,103 +59,14 @@ static void _ClearOathStatDebuf(struct Unit * unit)
     ClearUnitStatDebuff(unit, UNIT_STAT_BUFF_OATH_MOV);
 }
 
-bool ChapterInit_SetOathStatus(ProcPtr proc)
-{
-    int i, j;
-    struct Unit * unit;
-
-    for (i = FACTION_BLUE + 1; i < FACTION_BLUE + 1 + CONFIG_UNIT_AMT_ALLY; i++)
-    {
-        unit = GetUnit(i);
-        if (!UNIT_IS_VALID(unit))
-            continue;
-        
-        for (j = 0; j < ARRAY_COUNT_RANGE1x1; j++)
-        {
-            int _x = unit->xPos + gVecs_1x1[j].x;
-            int _y = unit->yPos + gVecs_1x1[j].y;
-
-            struct Unit * unit_ally = GetUnitAtPosition(_x, _y);
-            if (!UNIT_IS_VALID(unit_ally))
-                continue;
-
-            if (unit_ally->state & (US_HIDDEN | US_DEAD | US_RESCUED | US_BIT16))
-                continue;
-
-            if (AreUnitsAllied(unit->index, unit_ally->index))
-            {
-                _SetOathStatDebuf(unit);
-                break;
-            }
-        }
-    }
-
-    for (i = FACTION_GREEN + 1; i < FACTION_GREEN + 1 + CONFIG_UNIT_AMT_NPC; i++)
-    {
-        unit = GetUnit(i);
-        if (!UNIT_IS_VALID(unit))
-            continue;
-
-        for (j = 0; j < ARRAY_COUNT_RANGE1x1; j++)
-        {
-            int _x = unit->xPos + gVecs_1x1[j].x;
-            int _y = unit->yPos + gVecs_1x1[j].y;
-
-            struct Unit * unit_ally = GetUnitAtPosition(_x, _y);
-            if (!UNIT_IS_VALID(unit_ally))
-                continue;
-
-            if (unit_ally->state & (US_HIDDEN | US_DEAD | US_RESCUED | US_BIT16))
-                continue;
-
-            if (AreUnitsAllied(unit->index, unit_ally->index))
-            {
-                _SetOathStatDebuf(unit);
-                break;
-            }
-        }
-    }
-
-    for (i = FACTION_RED + 1; i < FACTION_RED + 1 + CONFIG_UNIT_AMT_ENEMY; i++)
-    {
-        unit = GetUnit(i);
-        if (!UNIT_IS_VALID(unit))
-            continue;
-
-        for (j = 0; j < ARRAY_COUNT_RANGE1x1; j++)
-        {
-            int _x = unit->xPos + gVecs_1x1[j].x;
-            int _y = unit->yPos + gVecs_1x1[j].y;
-
-            struct Unit * unit_ally = GetUnitAtPosition(_x, _y);
-            if (!UNIT_IS_VALID(unit_ally))
-                continue;
-
-            if (unit_ally->state & (US_HIDDEN | US_DEAD | US_RESCUED | US_BIT16))
-                continue;
-
-            if (AreUnitsAllied(unit->index, unit_ally->index))
-            {
-                _SetOathStatDebuf(unit);
-                break;
-            }
-        }
-    }
-
-    return false;
-}
-
 bool PrePhsae_TickOathSkillStatus(ProcPtr proc)
 {
     int i, j;
     struct Unit * unit;
-    int allies_gRange1_In1x1 = 0;
+    bool ally_in_range = false;
 
-    if (gPlaySt.faction == FACTION_BLUE)
+    for (i = gPlaySt.faction + 1; i < gPlaySt.faction + 0x40; ++i)
     {
-        for (i = FACTION_BLUE + 1; i < FACTION_BLUE + 1 + CONFIG_UNIT_AMT_ALLY; i++)
-        {
-            allies_gRange1_In1x1 = 0;
             unit = GetUnit(i);
             if (!UNIT_IS_VALID(unit))
                 continue;
@@ -174,80 +85,15 @@ bool PrePhsae_TickOathSkillStatus(ProcPtr proc)
 
                 if (AreUnitsAllied(unit->index, unit_ally->index))
                 {
-                    allies_gRange1_In1x1 += 1;
+                    ally_in_range = true;
                     break;
                 }
             }
 
-            if (allies_gRange1_In1x1 == 0)
+            if (ally_in_range)
                 _ClearOathStatDebuf(unit);
             else
                 _SetOathStatDebuf(unit);
-        }
-
-        for (i = FACTION_GREEN + 1; i < FACTION_GREEN + 1 + CONFIG_UNIT_AMT_NPC; i++)
-        {
-            allies_gRange1_In1x1 = 0;
-            unit = GetUnit(i);
-            if (!UNIT_IS_VALID(unit))
-                continue;
-
-           for (j = 0; j < ARRAY_COUNT_RANGE1x1; j++)
-            {
-                int _x = unit->xPos + gVecs_1x1[j].x;
-                int _y = unit->yPos + gVecs_1x1[j].y;
-
-                struct Unit * unit_ally = GetUnitAtPosition(_x, _y);
-                if (!UNIT_IS_VALID(unit_ally))
-                    continue;
-
-                if (unit_ally->state & (US_HIDDEN | US_DEAD | US_RESCUED | US_BIT16))
-                    continue;
-
-                if (AreUnitsAllied(unit->index, unit_ally->index))
-                {
-                    allies_gRange1_In1x1 += 1;
-                    break;
-                }
-            }
-
-            if (allies_gRange1_In1x1 == 0)
-                _ClearOathStatDebuf(unit);
-            else
-                _SetOathStatDebuf(unit);
-        }
-
-        for (i = FACTION_RED + 1; i < FACTION_RED + 1 + CONFIG_UNIT_AMT_ENEMY; i++)
-        {
-            allies_gRange1_In1x1 = 0;
-            unit = GetUnit(i);
-            if (!UNIT_IS_VALID(unit))
-                continue;
-
-            for (j = 0; j < ARRAY_COUNT_RANGE1x1; j++)
-            {
-                int _x = unit->xPos + gVecs_1x1[j].x;
-                int _y = unit->yPos + gVecs_1x1[j].y;
-
-                struct Unit * unit_ally = GetUnitAtPosition(_x, _y);
-                if (!UNIT_IS_VALID(unit_ally))
-                    continue;
-
-                if (unit_ally->state & (US_HIDDEN | US_DEAD | US_RESCUED | US_BIT16))
-                    continue;
-
-                if (AreUnitsAllied(unit->index, unit_ally->index))
-                {
-                    allies_gRange1_In1x1 += 1;
-                    break;
-                }
-            }
-
-            if (allies_gRange1_In1x1 == 0)
-                _ClearOathStatDebuf(unit);
-            else
-                _SetOathStatDebuf(unit);
-        }
     }
     return false;
 }
