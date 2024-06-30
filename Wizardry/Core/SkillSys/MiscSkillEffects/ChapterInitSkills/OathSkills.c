f6fb5d24d31a952c97b2b06b31bcc065a9d366e1
MokhaLeee <nkulyc@163.com>
2024-06-30 04:12:24 +0000

    update code style

--- Wizardry/Core/SkillSys/MiscSkillEffects/ChapterInitSkills/OathSkills.c
+++ Wizardry/Core/SkillSys/MiscSkillEffects/ChapterInitSkills/OathSkills.c
@@ -67,33 +67,33 @@ bool PrePhsae_TickOathSkillStatus(ProcPtr proc)
 
     for (i = gPlaySt.faction + 1; i < gPlaySt.faction + 0x40; ++i)
     {
-            unit = GetUnit(i);
-            if (!UNIT_IS_VALID(unit))
+        unit = GetUnit(i);
+        if (!UNIT_IS_VALID(unit))
+            continue;
+
+        for (j = 0; j < ARRAY_COUNT_RANGE1x1; j++)
+        {
+            int _x = unit->xPos + gVecs_1x1[j].x;
+            int _y = unit->yPos + gVecs_1x1[j].y;
+
+            struct Unit * unit_ally = GetUnitAtPosition(_x, _y);
+            if (!UNIT_IS_VALID(unit_ally))
                 continue;
 
-            for (j = 0; j < ARRAY_COUNT_RANGE1x1; j++)
+            if (unit_ally->state & (US_HIDDEN | US_DEAD | US_RESCUED | US_BIT16))
+                continue;
+
+            if (AreUnitsAllied(unit->index, unit_ally->index))
             {
-                int _x = unit->xPos + gVecs_1x1[j].x;
-                int _y = unit->yPos + gVecs_1x1[j].y;
-
-                struct Unit * unit_ally = GetUnitAtPosition(_x, _y);
-                if (!UNIT_IS_VALID(unit_ally))
-                    continue;
-
-                if (unit_ally->state & (US_HIDDEN | US_DEAD | US_RESCUED | US_BIT16))
-                    continue;
-
-                if (AreUnitsAllied(unit->index, unit_ally->index))
-                {
-                    ally_in_range = true;
-                    break;
-                }
+                ally_in_range = true;
+                break;
             }
+        }
 
-            if (ally_in_range)
-                _ClearOathStatDebuf(unit);
-            else
-                _SetOathStatDebuf(unit);
+        if (ally_in_range)
+            _ClearOathStatDebuf(unit);
+        else
+            _SetOathStatDebuf(unit);
     }
     return false;
 }
