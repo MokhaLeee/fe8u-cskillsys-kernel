19006adfd88f88e796aea186356ee1152070df2b
JesterWizard <164426396+JesterWizard@users.noreply.github.com>
2024-07-01 22:05:41 +0100

    Skill - SwiftSwim

--- Wizardry/Core/BattleSys/Source/PreBattleCalc.c
+++ Wizardry/Core/BattleSys/Source/PreBattleCalc.c
@@ -1051,6 +1051,13 @@ void PreBattleCalcSkills(struct BattleUnit * attacker, struct BattleUnit * defen
 
             break;
 #endif
+
+#if (defined(SID_SwiftSwim) && (COMMON_SKILL_VALID(SID_SwiftSwim)))
+        case SID_SwiftSwim:
+            if(gPlaySt.chapterWeatherId == WEATHER_RAIN)
+                attacker->battleSpeed *= 2;
+            break;
+#endif
         }
     }
 
