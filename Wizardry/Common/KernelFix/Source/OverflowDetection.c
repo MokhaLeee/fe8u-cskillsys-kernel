a08ddab07144f76c7f5e36f4ab4c843e00aba955
MokhaLeee <nkulyc@163.com>
2024-07-01 16:45:33 +0800

    fix spell issue

--- Wizardry/Common/KernelFix/Source/OverflowDetection.c
+++ Wizardry/Common/KernelFix/Source/OverflowDetection.c
@@ -6,7 +6,7 @@ extern u8 EwramOverlay0_FreeRamSpaceTop[], EwramOverlay0_UsedFreeRamSpaceTop[];
 
 void GameInit_OverflowDetection(void)
 {
-    /* Free RAN space */
+    /* Free RAM space */
     Assert(&FreeRamSpaceTop[0] < &UsedFreeRamSpaceTop[0]);
     Assert(&FreeRamSpace2Top[0] < &UsedFreeRamSpace2Top[0]);
     Assert(&EwramOverlay0_FreeRamSpaceTop[0] < &EwramOverlay0_UsedFreeRamSpaceTop[0]);
