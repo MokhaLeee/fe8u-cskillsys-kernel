6d2f8f4e0f9f74acb92263f00c23613922d25051
MokhaLeee <nkulyc@163.com>
2024-07-02 11:09:54 +0800

    split data section

---
+++ Wizardry/Core/CombatArt/BKSELfx/Source/RText_.c
@@ -0,0 +1,33 @@
+#include "common-chax.h"
+#include "combat-art.h"
+#include "help-box.h"
+
+void HbPopuplate_CombatArtBKSEL(struct HelpBoxProc * proc)
+{
+    int cid = GetCombatArtInForce(gActiveUnit);
+    proc->mid = GetCombatArtDesc(cid);
+
+    sHelpBoxType = NEW_HB_COMBAT_ART_BKSEL;
+}
+
+void HbPopuplate_NotCombatArtBKSEL(struct HelpBoxProc * proc)
+{
+    sHelpBoxType = 0;
+}
+
+void HbRedirect_CombatArtBKSEL(struct HelpBoxProc * proc)
+{
+    int cid = GetCombatArtInForce(gActiveUnit);
+    if (COMBART_VALID(cid))
+        return;
+
+    switch (proc->moveKey) {
+    case DPAD_DOWN:
+        TryRelocateHbDown(proc);
+        break;
+
+    case DPAD_UP:
+        TryRelocateHbUp(proc);
+        break;
+    }
+}
