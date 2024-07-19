94b0519731e07dbf1611ac433f2259ce1ed6bf2c
MokhaLeee <nkulyc@163.com>
2024-07-19 10:55:09 +0800

    add skill info for #233 #234

--- Data/SkillSys/SkillAnimInfo.c
+++ Data/SkillSys/SkillAnimInfo.c
@@ -323,4 +323,20 @@ const struct SkillAnimInfo gSkillAnimInfos[MAX_SKILL_NUM + 1] = {
         .sfx = 0x3D1,
     },
 #endif
+
+#if (defined(SID_Petrify) && COMMON_SKILL_VALID(SID_Petrify))
+    [SID_Petrify] = {
+        .aid = EFX_SKILL_DEFAULT,
+        .priority = EFX_PRIORITY_NORMAL,
+        .sfx = 0x3D1,
+    },
+#endif
+
+#if (defined(SID_Enrage) && COMMON_SKILL_VALID(SID_Enrage))
+    [SID_Enrage] = {
+        .aid = EFX_SKILL_DEFAULT,
+        .priority = EFX_PRIORITY_NORMAL,
+        .sfx = 0x3D1,
+    },
+#endif
 };
