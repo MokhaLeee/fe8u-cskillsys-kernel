a931ac5fb6ce6a79306b1be23b3560eb5ffd5a97
MokhaLeee <nkulyc@163.com>
2024-07-21 04:07:03 +0000

    fix summon

--- Wizardry/Core/SkillSys/MiscSkillEffects/MenuSkills/Summon.c
+++ Wizardry/Core/SkillSys/MiscSkillEffects/MenuSkills/Summon.c
@@ -6,9 +6,6 @@ u8 SummonCommandUsability(const struct MenuItemDef * def, int number)
     if (gActiveUnit->state & US_CANTOING)
         return MENU_NOTSHOWN;
 
-    if (gActiveUnit->state & US_HAS_MOVED)
-        return MENU_NOTSHOWN;
-
     MakeTargetListForSummon(gActiveUnit);
     if (GetSelectTargetCount() == 0)
         return MENU_NOTSHOWN;
