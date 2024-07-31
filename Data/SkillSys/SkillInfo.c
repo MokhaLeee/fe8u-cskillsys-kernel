cb8af23849a5248c5abf39b4c29d0907005883b3
JesterWizard <164426396+JesterWizard@users.noreply.github.com>
2024-07-31 18:38:06 +0100

    Skill - Pursuer

--- Data/SkillSys/SkillInfo.c
+++ Data/SkillSys/SkillInfo.c
@@ -3456,4 +3456,11 @@ const struct SkillInfo gSkillInfos[MAX_SKILL_NUM + 1] = {
         .icon = GFX_SkillIcon_WIP,
     },
 #endif
+
+#if (defined(SID_Pursuer) && COMMON_SKILL_VALID(SID_Pursuer))
+    [SID_Pursuer] = {
+        .desc = MSG_SKILL_Pursuer,
+        .icon = GFX_SkillIcon_Pursuer,
+    },
+#endif
 };
