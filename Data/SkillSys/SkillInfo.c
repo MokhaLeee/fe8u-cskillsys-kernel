ee91663a9d098ea010a8b736e1104974e944e662
JesterWizard <164426396+JesterWizard@users.noreply.github.com>
2024-08-11 00:55:18 +0100

    Skill - GaleWings

--- Data/SkillSys/SkillInfo.c
+++ Data/SkillSys/SkillInfo.c
@@ -3516,4 +3516,11 @@ const struct SkillInfo gSkillInfos[MAX_SKILL_NUM + 1] = {
         .icon = GFX_SkillIcon_WIP,
     },
 #endif
+
+#if (defined(SID_GaleWings) && COMMON_SKILL_VALID(SID_GaleWings))
+    [SID_GaleWings] = {
+        .desc = MSG_SKILL_GaleWings,
+        .icon = GFX_SkillIcon_WIP,
+    },
+#endif
 };
