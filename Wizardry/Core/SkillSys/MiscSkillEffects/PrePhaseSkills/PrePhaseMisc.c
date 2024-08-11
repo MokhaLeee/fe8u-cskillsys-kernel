f689504231540cb65c225bf6d6ab081d9f81aee1
MokhaLeee <nkulyc@163.com>
2024-08-11 16:32:58 +0000

    update UES

--- Wizardry/Core/SkillSys/MiscSkillEffects/PrePhaseSkills/PrePhaseMisc.c
+++ Wizardry/Core/SkillSys/MiscSkillEffects/PrePhaseSkills/PrePhaseMisc.c
@@ -6,29 +6,7 @@ bool PrePhsae_ClearMiscUES(ProcPtr proc)
     int uid;
     struct Unit * unit;
 
-    for (uid = FACTION_BLUE + 1; uid < FACTION_BLUE + 1 + CONFIG_UNIT_AMT_ALLY; uid++)
-    {
-        unit = GetUnit(uid);
-        if (UNIT_IS_VALID(unit))
-        {
-            ClearBitUES(unit, UES_BIT_GUARDBEAR_SKILL_USED);
-            ClearBitUES(unit, UES_BIT_TSZUKU_SKILL_USED);
-            ClearBitUES(unit, UES_BIT_TELEPOTRATION_SKILL_USED);
-        }
-    }
-
-    for (uid = FACTION_GREEN + 1; uid < FACTION_GREEN + 1 + CONFIG_UNIT_AMT_ALLY; uid++)
-    {
-        unit = GetUnit(uid);
-        if (UNIT_IS_VALID(unit))
-        {
-            ClearBitUES(unit, UES_BIT_GUARDBEAR_SKILL_USED);
-            ClearBitUES(unit, UES_BIT_TSZUKU_SKILL_USED);
-            ClearBitUES(unit, UES_BIT_TELEPOTRATION_SKILL_USED);
-        }
-    }
-
-    for (uid = FACTION_RED + 1; uid < FACTION_RED + 1 + CONFIG_UNIT_AMT_ALLY; uid++)
+    for (uid = 1; uid < 0xC0; uid++)
     {
         unit = GetUnit(uid);
         if (UNIT_IS_VALID(unit))
