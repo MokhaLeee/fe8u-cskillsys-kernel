8a30f9f2f0c2f4497872543c6860bacfb87ab0b8
JesterWizard <164426396+JesterWizard@users.noreply.github.com>
2024-08-12 21:26:58 +0100

    Skill - FranticSwing

--- Data/SkillSys/SkillInfo.c
+++ Data/SkillSys/SkillInfo.c
@@ -3523,4 +3523,12 @@ const struct SkillInfo gSkillInfos[MAX_SKILL_NUM + 1] = {
         .icon = GFX_SkillIcon_WIP,
     },
 #endif
+
+#if (defined(SID_FranticSwing) && COMMON_SKILL_VALID(SID_FranticSwing))
+    [SID_FranticSwing] = {
+        .name = MSG_SKILL_FranticSwing_NAME,
+        .desc = MSG_SKILL_FranticSwing,
+        .icon = GFX_SkillIcon_WIP,
+    },
+#endif
 };
--- Wizardry/Core/BattleSys/Source/BattleCalcReal.c
+++ Wizardry/Core/BattleSys/Source/BattleCalcReal.c
@@ -188,6 +188,18 @@ void ComputeBattleUnitEffectiveHitRate(struct BattleUnit * attacker, struct Batt
 
     if (attacker->battleEffectiveHitRate < 0)
         attacker->battleEffectiveHitRate = 0;
+
+#if (defined(SID_FranticSwing) && (COMMON_SKILL_VALID(SID_FranticSwing)))
+    if (BattleSkillTester(attacker, SID_FranticSwing))
+    {
+        NoCashGBAPrint("Frantic swing check has passed");
+        if(attacker->battleEffectiveHitRate <= 50)
+        {
+            NoCashGBAPrint("Effective hit rate is less then 50%");
+            attacker->battleCritRate += 50;
+        }
+    }
+#endif
 }
 
 /* LynJump */
