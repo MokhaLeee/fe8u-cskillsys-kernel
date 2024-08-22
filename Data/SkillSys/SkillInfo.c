5fc6fadce03edaafbd52e0fa9df2917ca5ff19d6
JesterWizard <adapps101@gmail.com>
2024-08-22 21:34:02 +0100

    Skill - Mimic

--- Data/SkillSys/SkillInfo.c
+++ Data/SkillSys/SkillInfo.c
@@ -3631,4 +3631,11 @@ const struct SkillInfo gSkillInfos[MAX_SKILL_NUM + 1] = {
         .icon = GFX_SkillIcon_VigorDance,
     },
 #endif
+
+#if (defined(SID_Mimic) && COMMON_SKILL_VALID(SID_Mimic))
+    [SID_Mimic] = {
+        .desc = MSG_SKILL_Mimic,
+        .icon = GFX_SkillIcon_WIP,
+    },
+#endif
 };
