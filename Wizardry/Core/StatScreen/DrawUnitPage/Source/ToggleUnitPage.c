4ac7635b0fad3d45d4bd8d3d33ff95707d456980
JesterWizard <adapps101@gmail.com>
2024-08-30 13:35:58 +0100

    PairUp stat screen rescue icon fix

--- Wizardry/Core/StatScreen/DrawUnitPage/Source/ToggleUnitPage.c
+++ Wizardry/Core/StatScreen/DrawUnitPage/Source/ToggleUnitPage.c
@@ -21,12 +21,12 @@ static inline void _growth_disp(int x, int y, int growth)
 
 STATIC_DECLAR void ToggleUnitPageGrowth(void)
 {
-    struct Unit * unit = gStatScreen.unit;
+    struct Unit *unit = gStatScreen.unit;
 
-    _growth_disp(18, 3,  GetUnitPowGrowth(unit));
-    _growth_disp(18, 5,  GetUnitMagGrowth(unit));
-    _growth_disp(18, 7,  GetUnitSklGrowth(unit));
-    _growth_disp(18, 9,  GetUnitSpdGrowth(unit));
+    _growth_disp(18, 3, GetUnitPowGrowth(unit));
+    _growth_disp(18, 5, GetUnitMagGrowth(unit));
+    _growth_disp(18, 7, GetUnitSklGrowth(unit));
+    _growth_disp(18, 9, GetUnitSpdGrowth(unit));
     _growth_disp(18, 11, GetUnitLckGrowth(unit));
     _growth_disp(18, 13, GetUnitDefGrowth(unit));
     _growth_disp(18, 15, GetUnitResGrowth(unit));
@@ -36,59 +36,57 @@ STATIC_DECLAR void ToggleUnitPageGrowth(void)
 
 STATIC_DECLAR void ToggleUnitPageBm(void)
 {
-    struct Unit * unit = gStatScreen.unit;
+    struct Unit *unit = gStatScreen.unit;
 
     DrawStatWithBarRework(0, 0x5, 0x1,
-                    gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    unit->pow,
-                    GetUnitPower(unit),
-                    UNIT_POW_MAX(unit));
+                          gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          unit->pow,
+                          GetUnitPower(unit),
+                          UNIT_POW_MAX(unit));
 
     DrawStatWithBarRework(1, 0x5, 0x3,
-                    gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    UNIT_MAG(unit),
-                    GetUnitMagic(unit),
-                    GetUnitMaxMagic(unit));
+                          gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          UNIT_MAG(unit),
+                          GetUnitMagic(unit),
+                          GetUnitMaxMagic(unit));
 
     DrawStatWithBarRework(2, 0x5, 0x5,
-                    gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    unit->skl,
-                    GetUnitSkill(unit),
-                    UNIT_SKL_MAX(unit));
+                          gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          unit->skl,
+                          GetUnitSkill(unit),
+                          UNIT_SKL_MAX(unit));
 
     DrawStatWithBarRework(3, 0x5, 0x7,
-                    gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    unit->spd,
-                    GetUnitSpeed(unit),
-                    UNIT_SPD_MAX(unit));
+                          gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          unit->spd,
+                          GetUnitSpeed(unit),
+                          UNIT_SPD_MAX(unit));
 
     DrawStatWithBarRework(4, 0x5, 0x9,
-                    gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    unit->lck,
-                    GetUnitLuck(unit),
-                    UNIT_LCK_MAX(unit));
+                          gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          unit->lck,
+                          GetUnitLuck(unit),
+                          UNIT_LCK_MAX(unit));
 
     DrawStatWithBarRework(5, 0x5, 0xB,
-                    gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    unit->def,
-                    GetUnitDefense(unit),
-                    UNIT_DEF_MAX(unit));
+                          gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          unit->def,
+                          GetUnitDefense(unit),
+                          UNIT_DEF_MAX(unit));
 
     DrawStatWithBarRework(6, 0x5, 0xD,
-                    gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
-                    unit->res,
-                    GetUnitResistance(unit),
-                    UNIT_RES_MAX(unit));
+                          gBG0TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          gBG2TilemapBuffer + TILEMAP_INDEX(12, 2),
+                          unit->res,
+                          GetUnitResistance(unit),
+                          UNIT_RES_MAX(unit));
 }
-
-
 
 STATIC_DECLAR void ToggleUnitPage(bool toggle)
 {
@@ -107,11 +105,11 @@ STATIC_DECLAR void ToggleUnitPage(bool toggle)
 }
 
 LYN_REPLACE_CHECK(PageNumCtrl_DisplayBlinkIcons);
-void PageNumCtrl_DisplayBlinkIcons(struct StatScreenPageNameProc * proc)
+void PageNumCtrl_DisplayBlinkIcons(struct StatScreenPageNameProc *proc)
 {
     bool blinking;
     bool saviour_pairup = false;
-    static const u16 palidLut[3] = { 0xC, 0xE, 0xD }; // TODO: palid constants
+    static const u16 palidLut[3] = {0xC, 0xE, 0xD}; // TODO: palid constants
 
     /* No idle in transition */
     if (gStatScreen.inTransition)
@@ -121,28 +119,31 @@ void PageNumCtrl_DisplayBlinkIcons(struct StatScreenPageNameProc * proc)
 
     if (gStatScreen.page == STATSCREEN_PAGE_0)
     {
-#if (defined(SID_PairUp) && (COMMON_SKILL_VALID(SID_PairUp)))
-    if (SkillTester(gStatScreen.unit, SID_PairUp))
-        saviour_pairup = true;
-#endif
-        if (gStatScreen.unit->state & US_RESCUING & !saviour_pairup)
+        if (gStatScreen.unit->state & US_RESCUING)
         {
-            UpdateStatArrowSprites(120, 56, 1);
-            UpdateStatArrowSprites(120, 72, 1);
+#if (defined(SID_PairUp) && (COMMON_SKILL_VALID(SID_PairUp)))
+            if (SkillTester(gStatScreen.unit, SID_PairUp))
+                saviour_pairup = true;
+#endif
+            if (!saviour_pairup)
+            {
+                UpdateStatArrowSprites(120, 56, 1);
+                UpdateStatArrowSprites(120, 72, 1);
+            }
 
             if (blinking)
             {
                 if (gStatScreenStExpa.talkee == 0)
                 {
                     PutSprite(4,
-                        184, 94, gObject_8x8,
-                        TILEREF(3, 0xF & palidLut[gStatScreen.unit->rescue >> 6]) + OAM2_LAYER(2));
+                              184, 94, gObject_8x8,
+                              TILEREF(3, 0xF & palidLut[gStatScreen.unit->rescue >> 6]) + OAM2_LAYER(2));
                 }
                 else
                 {
                     PutSprite(4,
-                        28, 86, gObject_8x8,
-                        TILEREF(3, 0xF & palidLut[gStatScreen.unit->rescue>>6]) + OAM2_LAYER(2));
+                              28, 86, gObject_8x8,
+                              TILEREF(3, 0xF & palidLut[gStatScreen.unit->rescue >> 6]) + OAM2_LAYER(2));
                 }
             }
         }
@@ -153,8 +154,8 @@ void PageNumCtrl_DisplayBlinkIcons(struct StatScreenPageNameProc * proc)
         if (blinking)
         {
             PutSprite(4,
-                28, 86, gObject_8x8,
-                TILEREF(3, 0xF & palidLut[gStatScreen.unit->rescue>>6]) + OAM2_LAYER(2));
+                      28, 86, gObject_8x8,
+                      TILEREF(3, 0xF & palidLut[gStatScreen.unit->rescue >> 6]) + OAM2_LAYER(2));
         }
     }
 
@@ -162,7 +163,8 @@ void PageNumCtrl_DisplayBlinkIcons(struct StatScreenPageNameProc * proc)
      * We direcly put page toggle here since here has been a hook during statscreen IDLE.
      * It's true that better to put such process into StatScreen_OnIdle(), but both are okay.
      */
-    switch (gpKernelDesigerConfig->unit_page_style) {
+    switch (gpKernelDesigerConfig->unit_page_style)
+    {
     case CONFIG_PAGE1_WITH_BWL:
     case CONFIG_PAGE1_WITH_LEADERSHIP:
         if ((gStatScreen.page == STATSCREEN_PAGE_0) && (FACTION_BLUE == UNIT_FACTION(gStatScreen.unit)))
@@ -203,7 +205,7 @@ void PageNumCtrl_DisplayBlinkIcons(struct StatScreenPageNameProc * proc)
 }
 
 LYN_REPLACE_CHECK(GlowBlendCtrl_OnLoop);
-void GlowBlendCtrl_OnLoop(struct StatScreenEffectProc * proc)
+void GlowBlendCtrl_OnLoop(struct StatScreenEffectProc *proc)
 {
     /**
      * Since BG1 may also consume chr resource on texts,
