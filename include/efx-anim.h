c401551e3fa73a131cce5c058cb949bc1e3328b2
MokhaLeee <nkulyc@163.com>
2024-09-02 13:21:48 +0000

    Fix #334

--- include/efx-anim.h
+++ include/efx-anim.h
@@ -7,7 +7,7 @@ struct EfxAnimConf {
      * If you want FEB identify your efxanim conf
      * then you have to add this unused header (dump from skillanimtemplate.dmp)
      */
-    u8 _unused_header[0x140];
+    u8 _unused_header[0x150];
 
     const s16 * frame_confs;
     u16 const * const * tsas;
