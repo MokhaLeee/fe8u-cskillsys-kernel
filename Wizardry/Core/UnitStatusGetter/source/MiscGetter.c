cbee900f410c15ec2c3d8de68f45c661eed86866
JesterWizard <adapps101@gmail.com>
2024-09-08 11:15:52 +0100

    Skill-IdentityProblems

--- Wizardry/Core/UnitStatusGetter/source/MiscGetter.c
+++ Wizardry/Core/UnitStatusGetter/source/MiscGetter.c
@@ -3,14 +3,14 @@
 #include "status-getter.h"
 #include "constants/skills.h"
 
-int StatusGetterCheckCpas(int status, struct Unit * unit)
+int StatusGetterCheckCpas(int status, struct Unit *unit)
 {
     LIMIT_AREA(status, 0, 127);
     return status;
 }
 
 LYN_REPLACE_CHECK(GetUnitMagBy2Range);
-int GetUnitMagBy2Range(struct Unit * unit)
+int GetUnitMagBy2Range(struct Unit *unit)
 {
     int range;
 
@@ -26,7 +26,7 @@ int GetUnitMagBy2Range(struct Unit * unit)
 }
 
 LYN_REPLACE_CHECK(GetUnitCurrentHp);
-int GetUnitCurrentHp(struct Unit * unit)
+int GetUnitCurrentHp(struct Unit *unit)
 {
     if (unit->curHP > GetUnitMaxHp(unit))
         unit->curHP = GetUnitMaxHp(unit);
@@ -35,15 +35,17 @@ int GetUnitCurrentHp(struct Unit * unit)
 }
 
 LYN_REPLACE_CHECK(IsPositionMagicSealed);
-s8 IsPositionMagicSealed(int x, int y) {
+s8 IsPositionMagicSealed(int x, int y)
+{
     int i;
 
-/**
- * Decomp set i to 0x81 initially, but we're now making
- * magic seal a skill, so we have to check player units too
- */
-    for (i = 0x1; i < 0xC0; ++i) {
-        struct Unit* unit = GetUnit(i);
+    /**
+     * Decomp set i to 0x81 initially, but we're now making
+     * magic seal a skill, so we have to check player units too
+     */
+    for (i = 0x1; i < 0xC0; ++i)
+    {
+        struct Unit *unit = GetUnit(i);
 
         if (!UNIT_IS_VALID(unit))
             continue;
@@ -56,7 +58,6 @@ s8 IsPositionMagicSealed(int x, int y) {
 #endif
         }
 
-
         if (RECT_DISTANCE(unit->xPos, unit->yPos, x, y) <= 10)
             return TRUE;
     }
@@ -65,7 +66,8 @@ s8 IsPositionMagicSealed(int x, int y) {
 }
 
 LYN_REPLACE_CHECK(IsUnitMagicSealed);
-s8 IsUnitMagicSealed(struct Unit* unit) {
+s8 IsUnitMagicSealed(struct Unit *unit)
+{
     if (unit->statusIndex == UNIT_STATUS_SILENCED)
         return TRUE;
 
@@ -73,4 +75,47 @@ s8 IsUnitMagicSealed(struct Unit* unit) {
         return TRUE;
 
     return FALSE;
+}
+
+LYN_REPLACE_CHECK(GetUnitPortraitId);
+inline int GetUnitPortraitId(struct Unit *unit)
+{
+
+#if (defined(SID_IdentityProblems) && (COMMON_SKILL_VALID(SID_IdentityProblems)))
+    if (SkillTester(unit, SID_IdentityProblems))
+        return NextRN_N(100);
+#endif
+
+    if (unit->pCharacterData->portraitId)
+    {
+        // TODO: PORTRAIT_LYON?, CHAPTER definitions
+        if (gPlaySt.chapterIndex == 0x22 && unit->pCharacterData->portraitId == 0x4A)
+            return 0x46;
+
+        return unit->pCharacterData->portraitId;
+    }
+
+    if (unit->pClassData->defaultPortraitId)
+        return unit->pClassData->defaultPortraitId;
+
+    return 0;
+}
+
+LYN_REPLACE_CHECK(GetUnitMiniPortraitId);
+inline int GetUnitMiniPortraitId(struct Unit *unit)
+{
+    int miniMugId = 0;
+
+#if (defined(SID_IdentityProblems) && (COMMON_SKILL_VALID(SID_IdentityProblems)))
+    if (SkillTester(unit, SID_IdentityProblems))
+        do
+        {
+            miniMugId = NextRN_N(100);
+        } while (miniMugId == 0);
+#endif
+
+    if (unit->pCharacterData->miniPortrait)
+        return 0x7F00 + unit->pCharacterData->miniPortrait;
+
+    return GetUnitPortraitId(unit);
 }
