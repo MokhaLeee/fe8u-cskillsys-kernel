0aaf59795a786bfed77d3acd4ae579af00343f89
MokhaLeee <nkulyc@163.com>
2024-09-10 10:51:50 +0800

    item jpt fix

--- Wizardry/Core/SkillSys/SkillScroll/Source/ItemSysJpt.s
+++ Wizardry/Core/SkillSys/SkillScroll/Source/ItemSysJpt.s
@@ -1,16 +1,16 @@
-    .INCLUDE "macro.inc"
+    .INCLUDE "macros.inc"
 
 THUMB_FUNC_START ItemUseEffectJpt_SkillScroll
 ItemUseEffectJpt_SkillScroll:
     mov r0, r5
-    bl ItemUseEffect_SkillScroll
+    blh ItemUseEffect_SkillScroll
     ldr r0, =0x802905C + 1
     bx r0
 
 THUMB_FUNC_START ItemUseActionJpt_SkillScroll
 ItemUseActionJpt_SkillScroll:
     mov r0, r6
-    bl ItemUseAction_SkillScroll
+    blh ItemUseAction_SkillScroll
     ldr r0, =0x802FF76 + 1
     bx r0
 
@@ -18,6 +18,6 @@ THUMB_FUNC_START ItemUsbilityJpt_SkillScroll
 ItemUsbilityJpt_SkillScroll:
     mov r0, r4
     mov r1, r5
-    bl ItemUsbility_SkillScroll
+    blh ItemUsbility_SkillScroll
     ldr r1, =0x8028C06 + 1
     bx r1
