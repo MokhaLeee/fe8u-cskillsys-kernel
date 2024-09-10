eafaea6357d426f2b72fb8a43486eb4d4f61ae9f
MokhaLeee <nkulyc@163.com>
2024-09-10 12:19:15 +0800

    add leadership patch

--- Wizardry/Common/KernelLib/Source/designer-config.c
+++ Wizardry/Common/KernelLib/Source/designer-config.c
@@ -52,6 +52,6 @@ const struct KernelDesigerConfig gKernelDesigerConfig = {
     .lvup_mode_normal   = CONFIG_LVUP_MODE_NORMAL,
     .lvup_mode_hard     = CONFIG_LVUP_MODE_HARD,
 
-    .unit_page_style = CONFIG_PAGE1_WITH_BWL,
-    .skil_page_style = CONFIG_PAGE4_MOKHA_PLAN_A,
+    .unit_page_style = CONFIG_PAGE1_WITH_LEADERSHIP,
+    .skil_page_style = CONFIG_PAGE4_MOKHA_PLAN_B,
 };
