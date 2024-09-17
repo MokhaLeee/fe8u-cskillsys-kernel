cc81bb4265e1eb6b87d206ce1dd79e33b899913d
JesterWizard <adapps101@gmail.com>
2024-09-17 22:25:55 +0100

    Skill-WaterWalker

--- Data/SkillSys/SkillInfo.c
+++ Data/SkillSys/SkillInfo.c
@@ -4172,4 +4172,11 @@ const struct SkillInfo gSkillInfos[MAX_SKILL_NUM + 1] = {
         .icon = GFX_SkillIcon_WIP,
     },
 #endif
+
+#if (defined(SID_WaterWalking) && COMMON_SKILL_VALID(SID_WaterWalking))
+    [SID_WaterWalking] = {
+        .desc = MSG_SKILL_WaterWalking,
+        .icon = GFX_SkillIcon_WIP,
+    },
+#endif
 };
