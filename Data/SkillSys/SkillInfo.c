11650a8e8eae31fb74a659b1c623e02d39b7596f
JesterWizard <adapps101@gmail.com>
2024-09-17 21:18:42 +0100

    Skill-Ludopathy

--- Data/SkillSys/SkillInfo.c
+++ Data/SkillSys/SkillInfo.c
@@ -4165,4 +4165,11 @@ const struct SkillInfo gSkillInfos[MAX_SKILL_NUM + 1] = {
         .icon = GFX_SkillIcon_WIP,
     },
 #endif
+
+#if (defined(SID_Ludopathy) && COMMON_SKILL_VALID(SID_Ludopathy))
+    [SID_Ludopathy] = {
+        .desc = MSG_SKILL_Ludopathy,
+        .icon = GFX_SkillIcon_WIP,
+    },
+#endif
 };
