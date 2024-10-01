0366174736f04a9a1e2fec044b572fbcb782beb1
JesterWizard <adapps101@gmail.com>
2024-10-01 12:08:24 +0100

    Skill-Survivor

--- Data/SkillSys/SkillInfo.c
+++ Data/SkillSys/SkillInfo.c
@@ -4326,4 +4326,11 @@ const struct SkillInfo gSkillInfos[MAX_SKILL_NUM + 1] = {
         .icon = GFX_SkillIcon_WIP,
     },
 #endif
+
+#if (defined(SID_Survivor) && COMMON_SKILL_VALID(SID_Survivor))
+    [SID_Survivor] = {
+        .desc = MSG_SKILL_Survivor,
+        .icon = GFX_SkillIcon_WIP,
+    },
+#endif
 };
