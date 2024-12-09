c486ece0fd2b6ca2820cf329c55675e1f87f9fcb
MokhaLeee <nkulyc@163.com>
2024-12-09 16:51:54 +0000

    Add anim number for EfxHpBarLive

---
+++ Wizardry/Common/BanimHack/AnimNumber/Source/EfxHpBarLive.c
@@ -0,0 +1,26 @@
+#include <common-chax.h>
+#include <banim-hack.h>
+#include <battle-system.h>
+
+static void EfxHPBarLiveRe_DisplayAnimNumber(struct ProcEfxHpBar *proc)
+{
+    struct Anim *anim = proc->anim_this;
+	int round = anim->nextRoundId - 1;
+	struct BattleHit *hit = (prBattleHitArray + BattleHitArrayWidth * round);
+
+	/**
+	 * Only called when hitted with damage
+	 */
+	NewEfxAnimNumber(anim, -hit->hpChange);
+}
+
+/* External hook */
+const struct ProcCmd ProcScr_efxHPBarLiveRework[] = {
+    PROC_NAME("efxHPBarLive"),
+    PROC_YIELD,
+    PROC_CALL(EfxHPBarLiveRe_DisplayAnimNumber),
+    PROC_REPEAT(EfxHPBarLiveMain),
+    PROC_REPEAT(EfxHpBar_MoveCameraOnEnd),
+    PROC_REPEAT(EfxHpBar_WaitCameraMove),
+    PROC_END
+};
