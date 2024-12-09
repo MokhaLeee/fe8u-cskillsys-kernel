cb7063c482ed9a940ab9485d59bd218ccd713463
MokhaLeee <nkulyc@163.com>
2024-12-09 12:18:23 +0800

    Fix CI

--- Data/GaidenMagic/GaidenMagicData.c
+++ Data/GaidenMagic/GaidenMagicData.c
@@ -40,9 +40,11 @@ struct GaidenPinfoConfig const gGaidenPinfoConfigList[0x100] = {
 
 // This config is valid only when CONFIG_GAIDEN_EXT_CONF_EN is set!
 const struct GaidenChaxConfigEnt gGaidenChaxConfigs[] = {
+#if (defined(SID_Latona) && COMMON_SKILL_VALID(SID_Latona))
 	{
 		.iid = ITEM_STAFF_LATONA,
 		.skill = SID_Latona,
 	},
+#endif
 	{ 0 },
 };
