3eabf44a4601d95f33dcae16075b12bdba43b283
MokhaLeee <nkulyc@163.com>
2024-12-10 17:52:56 +0800

    Add skill: Saint

--- Wizardry/Misc/GaidenMagic/Source/HpCost.c
+++ Wizardry/Misc/GaidenMagic/Source/HpCost.c
@@ -1,10 +1,17 @@
 #include <common-chax.h>
 #include <battle-system.h>
+#include <skill-system.h>
+#include <constants/skills.h>
 
 #include <gaiden-magic.h>
 
 int GetGaidenWeaponHpCost(struct Unit *unit, int item)
 {
+#if (defined(SID_Saint) && COMMON_SKILL_VALID(SID_Saint))
+	if (SkillTester(unit, SID_Saint))
+		return 0;
+#endif
+
 	return gpGaidenWeaponCostList[ITEM_INDEX(item)];
 }
 
