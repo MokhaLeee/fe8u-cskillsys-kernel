49e76fea1dfb2e6ccb5693a7f8a5e670048dbac4
MokhaLeee <nkulyc@163.com>
2024-12-10 10:37:44 +0800

    Fix code style

--- Wizardry/Common/BanimHack/AnimNumber/Source/EfxHpBarLive.c
+++ Wizardry/Common/BanimHack/AnimNumber/Source/EfxHpBarLive.c
@@ -4,7 +4,7 @@
 
 static void EfxHPBarLiveRe_DisplayAnimNumber(struct ProcEfxHpBar *proc)
 {
-    struct Anim *anim = proc->anim_this;
+	struct Anim *anim = proc->anim_this;
 	int round = anim->nextRoundId - 1;
 	struct BattleHit *hit = (prBattleHitArray + BattleHitArrayWidth * round);
 
@@ -16,11 +16,11 @@ static void EfxHPBarLiveRe_DisplayAnimNumber(struct ProcEfxHpBar *proc)
 
 /* External hook */
 const struct ProcCmd ProcScr_efxHPBarLiveRework[] = {
-    PROC_NAME("efxHPBarLive"),
-    PROC_YIELD,
-    PROC_CALL(EfxHPBarLiveRe_DisplayAnimNumber),
-    PROC_REPEAT(EfxHPBarLiveMain),
-    PROC_REPEAT(EfxHpBar_MoveCameraOnEnd),
-    PROC_REPEAT(EfxHpBar_WaitCameraMove),
-    PROC_END
+	PROC_NAME("efxHPBarLive"),
+	PROC_YIELD,
+	PROC_CALL(EfxHPBarLiveRe_DisplayAnimNumber),
+	PROC_REPEAT(EfxHPBarLiveMain),
+	PROC_REPEAT(EfxHpBar_MoveCameraOnEnd),
+	PROC_REPEAT(EfxHpBar_WaitCameraMove),
+	PROC_END
 };
