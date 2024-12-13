306c58829055ba15f1a3c5855b8dad765a2a1337
MokhaLeee <nkulyc@163.com>
2024-12-13 15:57:25 +0000

    Add ram space detection log

--- Wizardry/Common/OverflowDetection/OverflowDetection.c
+++ Wizardry/Common/OverflowDetection/OverflowDetection.c
@@ -2,12 +2,17 @@
 #include "skill-system.h"
 #include "battle-system.h"
 
-extern u8 FreeRamSpaceTop[], UsedFreeRamSpaceTop[];
-extern u8 FreeRamSpace2Top[], UsedFreeRamSpace2Top[];
-extern u8 EwramOverlay0_FreeRamSpaceTop[], EwramOverlay0_UsedFreeRamSpaceTop[];
+extern u8 FreeRamSpaceTop[], UsedFreeRamSpaceTop[], FreeRamSpaceBottom[];
+extern u8 FreeRamSpace2Top[], UsedFreeRamSpace2Top[], FreeRamSpace2Bottom[];
+extern u8 EwramOverlay0_FreeRamSpaceTop[], EwramOverlay0_UsedFreeRamSpaceTop[], EwramOverlay0_FreeRamSpaceBottom[];
 
 void GameInit_OverflowDetection(void)
 {
+	Print("Dump RAM space");
+	Printf("[1] top=0x%08X, used=%06X, free=0x%06X", UsedFreeRamSpaceTop, FreeRamSpaceBottom - UsedFreeRamSpaceTop, UsedFreeRamSpaceTop - FreeRamSpaceTop);
+	Printf("[2] top=0x%08X, used=%06X, free=0x%06X", UsedFreeRamSpace2Top, FreeRamSpace2Bottom - UsedFreeRamSpace2Top, UsedFreeRamSpace2Top - FreeRamSpace2Top);
+	Printf("[3] top=0x%08X, used=%06X, free=0x%06X", EwramOverlay0_UsedFreeRamSpaceTop, EwramOverlay0_FreeRamSpaceBottom - EwramOverlay0_UsedFreeRamSpaceTop, EwramOverlay0_UsedFreeRamSpaceTop - EwramOverlay0_FreeRamSpaceTop);
+
 	/* Free RAM space */
 	Assert(&FreeRamSpaceTop[0] < &UsedFreeRamSpaceTop[0]);
 	Assert(&FreeRamSpace2Top[0] < &UsedFreeRamSpace2Top[0]);
