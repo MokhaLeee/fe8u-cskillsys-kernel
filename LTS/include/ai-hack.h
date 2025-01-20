9c6e8020dfd028b8b29f036147af1dd8b305ca9d
MokhaLeee <nkulyc@163.com>
2025-01-20 11:41:32 +0800

    update debug config

--- LTS/include/ai-hack.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-enum ai_conf_expa {
-	AI_UNIT_CONFIG_EXTFLAG_TELEPORTATION = 1 << 8,
-};
--- LTS/include/hooks.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "common-chax.h"
-
-extern struct CharacterData const *const gpCharacterData;
-extern struct BattleHit *const prBattleHitArray;
