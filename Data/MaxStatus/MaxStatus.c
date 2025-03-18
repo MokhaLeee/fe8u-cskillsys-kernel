c8132520d059c52739f996bee5b1957f274da88e
MokhaLeee <nkulyc@163.com>
2025-03-18 12:55:22 +0000

    Add patch: modular unit max status

--- Data/MaxStatus/MaxStatus.c
+++ Data/MaxStatus/MaxStatus.c
@@ -1,4 +1,14 @@
 #include <common-chax.h>
 #include <status-getter.h>
 
-const struct UnitMaxStatusConfig gUnitMaxStatusConfigTable[0x100] = {};
+const struct UnitMaxStatusConfig gUnitMaxStatusConfigTable[0x100] = {
+	[CHARACTER_EIRIKA] = {
+		.pow = -2,
+		.mag = -2,
+		.spd = 5,
+		.skl = 5,
+		.lck = 5,
+		.def = -2,
+		.res = -2,
+	},
+};
