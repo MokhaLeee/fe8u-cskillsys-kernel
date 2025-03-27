5dc632fea044d59f11c754aa360f2b95b4c58f80
MokhaLeee <nkulyc@163.com>
2025-03-27 15:21:18 +0800

    split kernel secure data from none-secure data

--- include/Configs/config-memmap.h
+++ include/Configs/config-memmap.h
@@ -5,17 +5,21 @@
  * ------------------------------------------
  * Address  | Size  | Usage
  * ------------------------------------------
- * 080E8414 | 785A8 | Kernel (.section.text)
- * 08B2A604 | D59FC | Kernel (.section.data)
- * 08EFB2E0 | E4D20 | Font   (reserved)
- * 09000000 | ----  | DEMO
+ * 080E8414 | 785A8 | Kernel .text
+ * 089875AC | 0F000 | Kernel .rodata (secure)
+ * 08B2A604 | D59FC | user configurable .rodata (none-secure)
+ * 08EFB2E0 | E4D20 | Font .rodata
+ * 09000000 | ----  | _unused_
  * ------------------------------------------
  */
-#define FreeSpaceKernelText 0x0E8414
-#define FreeSpaceKernelTextSize 0x785A8
+#define KernelSpace_Text 0x0E8414
+#define KernelSpace_TextSize 0x785A8
 
-#define FreeSpaceKernel 0xB2A604
-#define FreeSpaceKernelSize 0xD59FC
+#define KernelSpace_Data 0x1C1EC0
+#define KernelSpace_DataSize 0x01408
+
+#define FreeSpaceCommon 0xB2A604
+#define FreeSpaceCommonSize 0xD59FC
 
 #define FreeSpaceFont 0xEFB2E0
 #define FreeSpaceFontSize 0xE4D20
@@ -24,7 +28,30 @@
 // #define FreeSpace FreeSpaceDEMO
 
 /**
- * Kernel memmap
+ * Secure rodata memmap
+ *
+ * ------------------------------
+ * Name      | Address | Size
+ * ------------------------------
+ * Magic     | 9875AC  | 00010
+ * Reloc     | 9875BC  | 00400
+ * Main      | 9879BC  | 0EBF0
+ * ------------------------------
+ */
+#define KernelMagicSize 0x10
+#define KernelRelocSize 0x400
+#define KernelKDataSize 0xEBF0
+
+#define KernelSpace_Magic (KernelSpace_Data + 0x0)
+#define KernelSpace_Reloc (KernelSpace_Data + KernelMagicSize)
+#define KernelSpace_KData (KernelSpace_Data + KernelMagicSize + KernelRelocSize)
+
+#define KernelSpace_MagicEnd (KernelSpace_Data + KernelMagicSize)
+#define KernelSpace_RelocEnd (KernelSpace_Data + KernelMagicSize + KernelRelocSize)
+#define KernelSpace_KDataEnd (KernelSpace_Data + KernelMagicSize + KernelRelocSize + KernelKDataSize)
+
+/**
+ * None-secure rodata memmap
  *
  * ------------------------------
  * Name      | Address | Size
@@ -35,19 +62,19 @@
  * MSG table | BF7FF4  | 0800C
  * ------------------------------
  */
-#define KernelMagicSize 0x10
-#define KernelRelocSize 0x400
-#define KernelDataSize  0xCD5E0
-#define MsgTableSize    0x800C
+#define UsrMagicSize 0x10
+#define UsrRelocSize 0x400
+#define UsrDataSize  0xCD5E0
+#define MsgTableSize 0x800C
 
-#define FreeSpaceKernel_Magic    (FreeSpaceKernel + 0x0)
-#define FreeSpaceKernel_Reloc    (FreeSpaceKernel + KernelMagicSize)
-#define FreeSpaceKernel_Data     (FreeSpaceKernel + KernelMagicSize + KernelRelocSize)
-#define FreeSpaceKernel_MsgTable (FreeSpaceKernel + KernelMagicSize + KernelRelocSize + KernelDataSize)
+#define FreeSpace_Magic    (FreeSpaceCommon + 0x0)
+#define FreeSpace_Reloc    (FreeSpaceCommon + UsrMagicSize)
+#define FreeSpace_UsrData  (FreeSpaceCommon + UsrMagicSize + UsrRelocSize)
+#define FreeSpace_MsgTable (FreeSpaceCommon + UsrMagicSize + UsrRelocSize + UsrDataSize)
 
-#define FreeSpaceKernel_MagicEnd    (FreeSpaceKernel + KernelMagicSize)
-#define FreeSpaceKernel_RelocEnd    (FreeSpaceKernel + KernelMagicSize + KernelRelocSize)
-#define FreeSpaceKernel_DataEnd     (FreeSpaceKernel + KernelMagicSize + KernelRelocSize + KernelDataSize)
-#define FreeSpaceKernel_MsgTableEnd (FreeSpaceKernel + KernelMagicSize + KernelRelocSize + KernelDataSize + MsgTableSize)
+#define FreeSpace_MagicEnd    (FreeSpaceCommon + UsrMagicSize)
+#define FreeSpace_RelocEnd    (FreeSpaceCommon + UsrMagicSize + UsrRelocSize)
+#define FreeSpace_UsrDataEnd  (FreeSpaceCommon + UsrMagicSize + UsrRelocSize + UsrDataSize)
+#define FreeSpace_MsgTableEnd (FreeSpaceCommon + UsrMagicSize + UsrRelocSize + UsrDataSize + MsgTableSize)
 
 #endif
