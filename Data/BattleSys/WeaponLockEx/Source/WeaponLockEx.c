98145f16d33a1e5c7bfe88959a4142aeca5c8d1f
MokhaLeee <nkulyc@163.com>
2025-03-28 18:05:39 +0800

    Add warn on weaponlock-ex

--- Data/BattleSys/WeaponLockEx/Source/WeaponLockEx.c
+++ Data/BattleSys/WeaponLockEx/Source/WeaponLockEx.c
@@ -1,10 +1,15 @@
 #include "common-chax.h"
 #include "weapon-lockex.h"
 
+/**
+ * This list has a very serious performance loss problem.
+ * Although this feature has not been removed from kernel,
+ * it is not recommended to abuse it.
+ *
+ * Call stack:
+ * CanUnitUseWeapon
+ * 	--> CheckWeaponLockEx
+ */
 const struct WeaponLockExConf gWeaponLockExConfs[] = {
-	{
-		.iid = ITEM_SWORD_RAPIER,
-		.pid = CHARACTER_EIRIKA,
-	},
 	{}
 };
