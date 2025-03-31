365ee8a19a9d4dfc736d1adecbaa8429352a52b4
JesterWizard <adapps101@gmail.com>
2025-03-31 22:55:01 +0100

    SKILL-ExplosiveBuff

--- Data/SkillSys/SkillTable-generic.c
+++ Data/SkillSys/SkillTable-generic.c
@@ -438,7 +438,7 @@ const struct SkillPreloadPConf gSkillPreloadPData[0x100] = {
     // 01
     [CHARACTER_EIRIKA] = {
         .skills = {
-            // [0] = SID_BanditBane,
+            // [0] = SID_ExplosiveBuff,
         },
     },
     // // 02
