fd8001553013828527614b6e6657ed6bd630b494
MokhaLeee <nkulyc@163.com>
2025-04-25 15:32:16 +0800

    Inject EXT spell-assoc

---
+++ Kernel/Wizardry/Common/ItemSys/SpellAssoc/ExtSpellAssoc/ExtSpellAssoc.c
@@ -0,0 +1,54 @@
+#include "common-chax.h"
+
+extern struct SpellAssoc ExtSpellAssocData[];
+extern struct SpellAssoc *pr_SpellAssocData;
+
+STATIC_DECLAR struct SpellAssoc *GetSpellAssocStructPtrVanilla(u16 item)
+{
+	struct SpellAssoc *it = pr_SpellAssocData;
+	u16 iid = GetItemIndex(item);
+	u16 item_;
+
+	item_ = it->item;
+	if (item_ != 0xFFFF)
+	{
+		while (item_ != iid)
+		{
+			item_ = (++it)->item;
+			if (item_ == 0xFFFF)
+				break;
+		}
+	}
+	return it;
+}
+
+STATIC_DECLAR struct SpellAssoc *GetSpellAssocStructPtrExt(u16 item)
+{
+	struct SpellAssoc *it = ExtSpellAssocData;
+	u16 iid = GetItemIndex(item);
+	u16 item_;
+
+	item_ = it->item;
+	if (item_ != 0xFFFF)
+	{
+		while (item_ != iid)
+		{
+			item_ = (++it)->item;
+			if (item_ == 0xFFFF)
+				break;
+		}
+	}
+	return it;
+}
+
+LYN_REPLACE_CHECK(GetSpellAssocStructPtr);
+struct SpellAssoc *GetSpellAssocStructPtr(u16 item)
+{
+	struct SpellAssoc *ret;
+
+	ret = GetSpellAssocStructPtrExt(item);
+	if (ret)
+		return ret;
+
+	return GetSpellAssocStructPtrVanilla(item);
+}
---
+++ Kernel/Wizardry/Common/ItemSys/SpellAssoc/ExtSpellAssoc/ExtSpellAssocData.c
@@ -0,0 +1,5 @@
+#include "common-chax.h"
+
+const struct SpellAssoc ExtSpellAssocData[] = {
+	SPELL_ASSOC_DATA_WPN_DEFAULT(0xFFFF),
+};
